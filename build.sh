d3071a04fce0ce9621e08d7d5b5ef269458209ce
sooti <sooti85@gmail.com>
2020-07-26 12:38:31 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -20,6 +20,10 @@ git fetch https://github.com/aosp-tissot/platform_frameworks_base-1.git Q
 git cherry-pick 7dcb550f5f267b4d275240c9d29c331cfedde42b
 git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/06/267306/21 && git cherry-pick FETCH_HEAD
 cd -
+cd packages/apps/Settings
+git fetch https://github.com/aosp-tissot/platform_packages_apps_settings-1.git Q
+git cherry-pick 0bca40ad2551843736e055e921443710c1d007da
+cd -
 if [ "$2" == "new" ];then
 cd device/phh/treble
 bash generate.sh
