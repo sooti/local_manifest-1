f0294a5607f0443abbf4086f43238b4c9e05fa4f
sooti <sooti85@gmail.com>
2020-07-26 12:46:36 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ repo init -u https://android.googlesource.com/platform/manifest -b android-10.0.
 if [ "$2" == "new" ];then
   wget https://github.com/aosp-tissot/local_manifest/raw/aosp-10.0/local_manifest.xml .repo/local_manifest.xml
   wget https://raw.githubusercontent.com/aosp-tissot/local_manifest/aosp-10.0/patch.sh
+  wget https://github.com/phhusson/treble_experimentations/releases/download/v217/patches.zip
 fi
 repo sync -c -j 16 -f --force-sync --no-tag --no-clone-bundle --optimized-fetch --prune
 if [ "$1" == "clean" ]
