d525f8bb33a93db0d0602e94d69a3a64afbbbb92
sooti <sooti85@gmail.com>
2020-07-26 12:33:31 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ repo forall -c 'git reset --hard ; git clean -fdx'
 repo init -u https://android.googlesource.com/platform/manifest -b android-10.0.0_r40
 if [ "$2" == "new" ];then
   wget https://github.com/aosp-tissot/local_manifest/raw/aosp-10.0/local_manifest.xml .repo/local_manifest.xml
+  wget https://raw.githubusercontent.com/aosp-tissot/local_manifest/aosp-10.0/patch.sh
 fi
 repo sync -c -j 16 -f --force-sync --no-tag --no-clone-bundle --optimized-fetch --prune
 if [ "$1" == "clean" ]
@@ -19,9 +20,11 @@ git fetch https://github.com/aosp-tissot/platform_frameworks_base-1.git Q
 git cherry-pick 7dcb550f5f267b4d275240c9d29c331cfedde42b
 git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/06/267306/21 && git cherry-pick FETCH_HEAD
 cd -
+if [ "$2" == "new" ];then
 cd device/phh/treble
 bash generate.sh
 cd -
+fi
 . build/envsetup.sh
 lunch treble_arm64_bgN-user
 if [ "$1" == "clean" ];then
