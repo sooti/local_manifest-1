9fcab0e3c6c5c7401c277f2120a7a856e77a7d4b
sooti <sooti85@gmail.com>
2020-08-05 09:12:31 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 set -e
-repo forall -c 'git reset --hard ; git clean -fdx' 
-repo init -u https://android.googlesource.com/platform/manifest -b android-10.0.0_r40
 if [ "$2" == "new" ];then
+  repo init -u https://android.googlesource.com/platform/manifest -b android-10.0.0_r41
   wget https://github.com/aosp-tissot/local_manifest/raw/aosp-10.0/local_manifest.xml .repo/local_manifest.xml
   wget https://raw.githubusercontent.com/aosp-tissot/local_manifest/aosp-10.0/patch.sh
   wget https://github.com/phhusson/treble_experimentations/releases/download/v217/patches.zip
   wget https://raw.githubusercontent.com/aosp-tissot/local_manifest/aosp-10.0/patch.txt
   unzip ./patches.zip
-fi
 repo sync -c -j 16 -f --force-sync --no-tag --no-clone-bundle --optimized-fetch --prune
-if [ "$1" == "clean" ]
+fi
+if [ "$1" == "clean" ];then
+  repo forall -c 'git reset --hard ; git clean -fdx'
   rm patches.zip
   rm -rf patches
   wget https://github.com/phhusson/treble_experimentations/releases/download/v217/patches.zip
   unzip ./patches.zip
+  repo sync -c -j 16 -f --force-sync --no-tag --no-clone-bundle --optimized-fetch --prune
 fi
 bash patch.sh ./
 patch -p1 < patch.txt
@@ -27,13 +28,23 @@ cd packages/apps/Settings
 git fetch https://github.com/aosp-tissot/platform_packages_apps_settings-1.git Q
 git cherry-pick 0bca40ad2551843736e055e921443710c1d007da
 cd -
-if [ "$2" == "new" ];then
 cd device/phh/treble
 bash generate.sh
 cd -
-fi
 . build/envsetup.sh
-lunch treble_arm64_bgN-user
+if [ "$2" == "arm64-gapps" ];then
+   lunch treble_arm64_bgN-user
+fi
+if [ "$2" == "arm64-gapps-go" ];then
+   lunch treble_arm64_boN-user
+fi
+if [ "$2" == "arm32-gapps-go" ];then
+treble_arm_aoN-user
+fi
+if [ "$2" == "arm64-vanilla" ];then
+treble_arm_aoN-user
+fi
+treble_arm64_bvN
 if [ "$1" == "clean" ];then
    make installclean
 fi
