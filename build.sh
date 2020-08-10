fcedd164864554dad18986f6196e67b4f1c552eb
sooti <sooti85@gmail.com>
2020-08-10 09:39:53 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ if [ "$1" == "clean" ];then
   repo forall -c 'git reset --hard ; git clean -fdx'
   rm patches.zip
   rm -rf patches
-  wget https://github.com/phhusson/treble_experimentations/releases/download/v217/patches.zip
+  wget https://github.com/phhusson/treble_experimentations/releases/download/v2/patches.zip
   unzip ./patches.zip
   repo sync -c -j 16 -f --force-sync --no-tag --no-clone-bundle --optimized-fetch --prune
 fi
@@ -24,10 +24,10 @@ cd frameworks/base
 git fetch https://github.com/aosp-tissot/platform_frameworks_base-1.git Q
 git cherry-pick 7dcb550f5f267b4d275240c9d29c331cfedde42b
 git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/06/267306/21 && git cherry-pick FETCH_HEAD
-cd -
-cd packages/apps/Settings
-git fetch https://github.com/aosp-tissot/platform_packages_apps_settings-1.git Q
-git cherry-pick 0bca40ad2551843736e055e921443710c1d007da
+# cd -
+# cd packages/apps/Settings
+# git fetch https://github.com/aosp-tissot/platform_packages_apps_settings-1.git Q
+# git cherry-pick 0bca40ad2551843736e055e921443710c1d007da
 cd -
 cd device/phh/treble
 bash generate.sh
