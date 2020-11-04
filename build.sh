68f2b5664c21e4fac14987748865948d7cc0bf2a
sooti <sooti85@gmail.com>
2020-11-04 12:13:15 +0200

    Add volte icon commits and update branche to nov

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 if [ "$1" == "new" ] || [ "$2" == "new" ];then
-  repo init -u https://android.googlesource.com/platform/manifest -b android-11.0.0_r5 --depth=1
+  repo init -u https://android.googlesource.com/platform/manifest -b android-11.0.0_r15 --depth=1
   wget https://github.com/aosp-tissot/local_manifest/raw/aosp-10.0/local_manifest.xml .repo/local_manifest.xml
   wget https://raw.githubusercontent.com/aosp-tissot/local_manifest/aosp-11.0/patch.sh
-  wget https://github.com/phhusson/treble_experimentations/releases/download/v300.f/patches.zip
+  wget https://github.com/phhusson/treble_experimentations/releases/download/v300.g/patches.zip
   wget https://raw.githubusercontent.com/aosp-tissot/local_manifest/aosp-11.0/patch.zip
   unzip ./patches.zip
   unzip ./patch.zip
@@ -26,7 +26,7 @@ if [ "$1" == "clean" ] || [ "$2" == "clean" ];then
   if [ -d "patches" ]; then
      rm -rf patches
   fi
-  wget https://github.com/phhusson/treble_experimentations/releases/download/v300.f/patches.zip
+  wget https://github.com/phhusson/treble_experimentations/releases/download/v300.g/patches.zip
   unzip ./patches.zip
   repo sync -c -j 16 -f --force-sync --no-tag --no-clone-bundle --optimized-fetch --prune
   bash patch.sh ./
@@ -34,6 +34,7 @@ if [ "$1" == "clean" ] || [ "$2" == "clean" ];then
   cd frameworks/base
   git fetch https://github.com/aosp-tissot/platform_frameworks_base-1.git R
   git cherry-pick cd1374b20efccf83140a9c67d6ca18dcb2f46c85 64e57deceede996181bb13d4990c09e17cb39c44
+  git cherry-pick f02d9c03ec0de0e33f8ada16e0789988828b6487^..d1e35f3c072aee2fe6e3dee4da7e9b067a66e72d
 #  git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/06/267306/21 && git cherry-pick FETCH_HEAD
   cd -
 fi
