6646b0ec1a8ef46adbecab0b7c06b98528c65316
sooti <sooti85@gmail.com>
2020-11-04 08:11:34 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 if [ "$1" == "new" ] || [ "$2" == "new" ];then
-  repo init -u https://android.googlesource.com/platform/manifest -b android-11.0.0_r5
-  wget https://github.com/aosp-tissot/local_manifest/raw/aosp-11.0/local_manifest.xml .repo/local_manifest.xml
+  repo init -u https://android.googlesource.com/platform/manifest -b android-11.0.0_r5 --depth=1
+  wget https://github.com/aosp-tissot/local_manifest/raw/aosp-10.0/local_manifest.xml .repo/local_manifest.xml
   wget https://raw.githubusercontent.com/aosp-tissot/local_manifest/aosp-11.0/patch.sh
   wget https://github.com/phhusson/treble_experimentations/releases/download/v300.f/patches.zip
   wget https://raw.githubusercontent.com/aosp-tissot/local_manifest/aosp-11.0/patch.zip
@@ -18,7 +18,7 @@ if [ "$1" == "new" ] || [ "$2" == "new" ];then
   cd -
 fi
 if [ "$1" == "clean" ] || [ "$2" == "clean" ];then
-  repo init -u https://android.googlesource.com/platform/manifest -b android-11.0.0_r5
+  repo init -u https://android.googlesource.com/platform/manifest -b android-11.0.0_r15
   repo forall -c 'git reset --hard ; git clean -fdx'
   if [ -f "patches.zip" ]; then
      rm patches.zip
@@ -31,18 +31,12 @@ if [ "$1" == "clean" ] || [ "$2" == "clean" ];then
   repo sync -c -j 16 -f --force-sync --no-tag --no-clone-bundle --optimized-fetch --prune
   bash patch.sh ./
   patch -p1 < patch.txt
-#  cd frameworks/base
-#  git fetch https://github.com/aosp-tissot/platform_frameworks_base-1.git Q
-#  git cherry-pick 7dcb550f5f267b4d275240c9d29c331cfedde42b
+  cd frameworks/base
+  git fetch https://github.com/aosp-tissot/platform_frameworks_base-1.git R
+  git cherry-pick cd1374b20efccf83140a9c67d6ca18dcb2f46c85 64e57deceede996181bb13d4990c09e17cb39c44
 #  git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/06/267306/21 && git cherry-pick FETCH_HEAD
-# cd -
+  cd -
 fi
-# cd -
-# cd packages/apps/Settings
-# git fetch https://github.com/aosp-tissot/platform_packages_apps_settings-1.git Q
-# git cherry-pick 0bca40ad2551843736e055e921443710c1d007da
-#cd -
-#patch -p1 < R_patch.txt
 cd device/phh/treble
 bash generate.sh
 cd -
@@ -62,5 +56,5 @@ fi
 if [ "$1" == "clean" ];then
    make installclean
 fi
-make -j16 systemimage
-xz -c -v ./out/target/product/phhgsi_arm64_ab/system.img -T8 > system.img.xz
+make -j4 systemimage
+xz -c -v ./out/target/product/phhgsi_arm64_ab/system.img -T4 > system.img.xz
