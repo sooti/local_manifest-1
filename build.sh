efd3fae157a9812c6b3763c683d4651c284feedd
sooti <sooti85@gmail.com>
2021-02-08 11:23:57 +0200

    Update build.sh
    Switch to patches instead of pulling commits each time

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 if [ "$1" == "new" ] || [ "$2" == "new" ];then
-  repo init -u https://android.googlesource.com/platform/manifest -b android-11.0.0_r21 --depth=1
+  repo init -u https://android.googlesource.com/platform/manifest -b android-11.0.0_r30 --depth=1
   wget https://github.com/aosp-tissot/local_manifest/raw/aosp-10.0/local_manifest.xml .repo/local_manifest.xml
   wget https://raw.githubusercontent.com/aosp-tissot/local_manifest/aosp-11.0/patch.sh
   wget https://github.com/phhusson/treble_experimentations/releases/download/v300.j/patches.zip
@@ -11,12 +11,13 @@ if [ "$1" == "new" ] || [ "$2" == "new" ];then
   repo sync -c -j 16 -f --force-sync --no-tag --no-clone-bundle --optimized-fetch --prune
   bash patch.sh ./
   patch -p1 < patch.txt
-  cd frameworks/base
-  git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/06/267306/21 && git cherry-pick FETCH_HEAD
+#  cd frameworks/base
+#  git fetch https://github.com/aosp-tissot/platform_frameworks_base-1.git R
+#  git cherry-pick aa3f0a92c9cbd31d0eb95114c22dc8484050fc7a^..113c0fa5546947b70a1288f8e798e71f3a0da326
   cd -
 fi
 if [ "$1" == "clean" ] || [ "$2" == "clean" ];then
-  repo init -u https://android.googlesource.com/platform/manifest -b android-11.0.0_r21
+  repo init -u https://android.googlesource.com/platform/manifest -b android-11.0.0_r30 --depth=1
   repo forall -c 'git reset --hard ; git clean -fdx'
   if [ -f "patches.zip" ]; then
      rm patches.zip
@@ -29,11 +30,11 @@ if [ "$1" == "clean" ] || [ "$2" == "clean" ];then
   repo sync -c -j 16 -f --force-sync --no-tag --no-clone-bundle --optimized-fetch --prune
   bash patch.sh ./
   patch -p1 < patch.txt
-  cd frameworks/base
-  git fetch https://github.com/aosp-tissot/platform_frameworks_base-1.git R
-  git cherry-pick cd1374b20efccf83140a9c67d6ca18dcb2f46c85 64e57deceede996181bb13d4990c09e17cb39c44
-  git cherry-pick f02d9c03ec0de0e33f8ada16e0789988828b6487^..d1e35f3c072aee2fe6e3dee4da7e9b067a66e72d
-  cd -
+#  cd frameworks/base
+#  git fetch https://github.com/aosp-tissot/platform_frameworks_base-1.git R
+#  git cherry-pick aa3f0a92c9cbd31d0eb95114c22dc8484050fc7a^..113c0fa5546947b70a1288f8e798e71f3a0da326
+#  git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/06/267306/21 && git cherry-pick FETCH_HEAD
+#  cd -
 fi
 cd device/phh/treble
 bash generate.sh
@@ -55,4 +56,5 @@ if [ "$1" == "clean" ];then
    make installclean
 fi
 make -j4 systemimage
-xz -c -v ./out/target/product/phhgsi_arm64_ab/system.img -T4 > system.img.xz
+threads = `cat /proc/cpuinfo | grep processor | wc -l`
+xz -c -v ./out/target/product/phhgsi_arm64_ab/system.img -T$threads > system.img.xz
