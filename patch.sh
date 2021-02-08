53ce23b5e6890a800cd74ff28e3d7d48f4720234
sooti <sooti85@gmail.com>
2021-02-08 11:28:27 +0200

    Add our patches zip
    Pull and apply our patches

--- patch.sh
+++ patch.sh
@@ -1,17 +1,17 @@
-
 #!/bin/bash
 
 set -e
 
 patches="$(readlink -f -- $1)"
-
-for project in $(cd $patches/patches; echo *);do
+wget https://github.com/aosp-tissot/local_manifest/blob/aosp-11.0/sooti-patches.zip
+unzip sooti-patches.zip
+for project in $(cd $patches/patches; echo *) $(cd $patches/sooti-patches; echo *) ;do
         p="$(tr _ / <<<$project |sed -e 's;platform/;;g')"
         [ "$p" == build ] && p=build/make
         repo sync -l --force-sync $p
         pushd $p
         git clean -fdx; git reset --hard
-        for patch in $patches/patches/$project/*.patch;do
+        for patch in $patches/patches/$project/*.patch $patches/sooti-patches/$project/*.patch;do
                 #Check if patch is already applied
                 if patch -f -p1 --dry-run -R < $patch > /dev/null;then
                         continue
