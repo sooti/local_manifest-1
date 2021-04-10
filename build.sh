5afb9d7198eed1baa0b0ec7652215b084e366561
sooti <sooti85@gmail.com>
2021-04-10 17:36:31 +0300

    Update to latest changes

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 if [ "$1" == "new" ] || [ "$2" == "new" ];then
-  repo init -u https://android.googlesource.com/platform/manifest -b android-11.0.0_r30 --depth=1
+  repo init -u https://android.googlesource.com/platform/manifest -b android-11.0.0_r34 --depth=1
   wget https://github.com/aosp-tissot/local_manifest/raw/aosp-10.0/local_manifest.xml .repo/local_manifest.xml
   wget https://raw.githubusercontent.com/aosp-tissot/local_manifest/aosp-11.0/patch.sh
-  wget https://github.com/phhusson/treble_experimentations/releases/download/v300.j/patches.zip
+  wget https://github.com/phhusson/treble_experimentations/releases/download/v304/patches.zip
   wget https://raw.githubusercontent.com/aosp-tissot/local_manifest/aosp-11.0/patch.zip
   unzip ./patches.zip
   unzip ./patch.zip
@@ -14,7 +14,7 @@ if [ "$1" == "new" ] || [ "$2" == "new" ];then
   cd -
 fi
 if [ "$1" == "clean" ] || [ "$2" == "clean" ];then
-  repo init -u https://android.googlesource.com/platform/manifest -b android-11.0.0_r30 --depth=1
+  repo init -u https://android.googlesource.com/platform/manifest -b android-11.0.0_r34 --depth=1
   repo forall -c 'git reset --hard ; git clean -fdx'
   if [ -f "patches.zip" ]; then
      rm patches.zip
@@ -22,7 +22,7 @@ if [ "$1" == "clean" ] || [ "$2" == "clean" ];then
   if [ -d "patches" ]; then
      rm -rf patches
   fi
-  wget https://github.com/phhusson/treble_experimentations/releases/download/v300.j/patches.zip
+  wget https://github.com/phhusson/treble_experimentations/releases/download/v304/patches.zip
   unzip ./patches.zip
   repo sync -c -j 16 -f --force-sync --no-tag --no-clone-bundle --optimized-fetch --prune
   bash patch.sh ./
@@ -47,6 +47,6 @@ fi
 if [ "$1" == "clean" ];then
    make installclean
 fi
-make -j4 systemimage
-threads = `cat /proc/cpuinfo | grep processor | wc -l`
+make -j3 systemimage
+threads = "cat /proc/cpuinfo | grep processor | wc -l"
 xz -c -v ./out/target/product/phhgsi_arm64_ab/system.img -T$threads > system.img.xz
